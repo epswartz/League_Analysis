7a84586adb24dbd2b64f96e2ac6a823a3220ca8c
epswartz <eswartzen@gmail.com>
2019-11-05 16:55:10 -0500

    fetcher, modify scraper, rescrape names

--- scrape.fish
+++ scrape.fish
@@ -1,7 +1,12 @@
 #!/usr/local/bin/fish
-set startIdx 593
+
+# Scrapes the leaderboard of op.gg for usernames.
+set startIdx 594 # As of 11/5/2019, the first page of all P1
+set skipFactor 10 # Reads from every skipFactorth page - we can spread out the reads so we don't have a continuous zone of the ladder
 for i in (seq 10)
-    set pageNumber (math $startIdx+$i)
+    set idx (math "$skipFactor * $i")
+    set pageNumber (math $startIdx+$idx)
     curl https://na.op.gg/ranking/ladder/page=$pageNumber > temp.html
     cat temp.html | grep -o 'userName=.*' | cut -f 2 -d = | cut -f 1 -d '"' >> names.txt
+    rm temp.html
 end
